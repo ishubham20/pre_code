308e9db8e42b5cdd0a6b0bee5640aa60b0aaad58
shubham20 <ishubhamb70@gmail.com>
2023-11-02 08:32:46 +0530

    kryword code

---
+++ keyword.js
@@ -0,0 +1,24 @@
+//keyword js 
+// let, var, const
+let cityName = 'kolhapur';
+ cityName = 'sangali';
+console.log(cityName); // output is sangali
+
+let // we can reassign many time but decleared only one time 
+a = 5;
+b = 4; 
+c = 10;
+console.log(a + b + c); // 19
+//
+  var carName = "audi";
+ var  carName = "tata";
+ var d = 10;
+ e = 5;
+ console.log(d+e); //15
+ 
+  console.log(carName); // we can redeclear and reassign the var keyword.
+  //
+  //const // we can not redeclear and reassign const keyword
+  const myName = "shubham"
+  console.log(myName) //shubham 
+
